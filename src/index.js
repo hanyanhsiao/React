72c2a2707bc7dc50826dbaf7a53f3eb40f54c72f
hanyanhsiao <j0955551237@gmail.com>
2022-11-27 17:24:16 +0800

    初始化專案，加入React相關套件

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from"react-dom";
+
+//利用Babel編譯器翻譯React JSX 語法
+let app=<div>好難喔</div>;//React JSX
+ReactDOM.render(app,document.getElementById("root"));
